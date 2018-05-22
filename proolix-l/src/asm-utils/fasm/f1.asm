f2c184092a8aded3e51fce221832a144c2a74d73
prool <proolix@gmail.com>
2018-05-22 16:39:30 +0300

    .

--- proolix-l/src/asm-utils/fasm/f1.asm
+++ proolix-l/src/asm-utils/fasm/f1.asm
@@ -4,16 +4,56 @@
 ; this is msdos-like .com file
 ; execute in Proolix with command 'rundos'
 ;
+; test of fn25h of int13h
+;
 ; /Prool
 ;
 	org	100H
 
-	mov	ah,0eh
-	mov	al,34
-	int	10h
+	push	ES	; save ES
 
-	mov	ah,0eh
-	mov	al,34
-	int	10h
+	push	DS
+	pop	ES	; ES:=DS
+
+;	mov	ah,0eh
+;	mov	al,'%'
+;	int	10h
+
+	mov	ah,9
+	mov	dx,string
+	int	21h
+
+	mov	ax,2500H
+	mov	dl,80H
+	mov	bx,buffer
+	int	13h
+	jc	l_err
+
+	mov	ah,9
+	mov	dx,s_ok
+	int	21h
+l_exit:
+	pop	ES	; restore ES
 
 	int	20h
+
+l_err:
+	call	ohw
+
+	mov	ah,9
+	mov	dx,s_err
+	int	21h
+
+	jmp	l_exit
+
+include		"ohw.asm"
+include		"ohb.asm"
+
+string:		db	"This is 1st fasm .com util for Proolix"
+		db	13,10,"$"
+
+s_ok:	db	"int13h OK$"
+
+s_err:	db	" - int13h err$"
+
+buffer:
---
+++ proolix-l/src/asm-utils/fasm/ohb.asm
@@ -0,0 +1,45 @@
+; ohb
+
+ohb:
+
+; Procedure output hex byte Ver 0.1.1 6 Dec 93 via BIOS
+; Input: AL - byte
+; All regs. reserved ;)
+
+; Not worked in graph mode. bl - bg color !!!
+
+        push    ax
+        push    cx
+        push    dx
+
+        mov     dl,al
+        mov     cl,4
+        shr     al,cl
+        call    ohb1
+
+        mov     al,dl
+        and     al,0fh
+        call    ohb1
+
+        pop     dx
+        pop     cx
+        pop     ax
+
+        ret
+
+ohb1:    ; Regs not saved !!!
+        push    ax
+
+        cmp     al,9
+        ja      @@_1    ; al > 9
+        ; al <= 9
+        add     al,'0'
+        jmp     @@_out
+
+@@_1:   add     al,'A'-10
+@@_out: mov     ah,0eh
+        int     10h
+
+        pop     ax
+
+        ret
